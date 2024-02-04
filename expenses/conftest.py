3a44344dc4a46ad68e01c7448ae6cf073969d8a0
Ian Andwati <andwatiian@gmail.com>
2024-02-04 16:22:42 +0300

    refactor project

---
+++ expenses/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from expenses.users.models import User
+from expenses.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
